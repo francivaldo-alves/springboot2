06f2ccf9ab2cdb1b543a41f08070a5b4785387eb
Francivaldo Alves <francivaldo.alves@gmail.com>
2022-06-27 21:11:08 -0300

    Criando o metodo findById e application.yml

--- src/main/java/com/f3pro/springboot2/controller/AnimeController.java
+++ src/main/java/com/f3pro/springboot2/controller/AnimeController.java
@@ -5,7 +5,9 @@ import com.f3pro.springboot2.service.AnimeService;
 import com.f3pro.springboot2.util.DateUltil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,8 +24,14 @@ public class AnimeController {
     private final AnimeService animeService;
 
     @GetMapping
-    public List<Anime> list(){
+    public ResponseEntity<List<Anime>>list(){
         log.info(dateUltil.formatLocalDateToDatebaseStylw(LocalDateTime.now()));
-        return  animeService.listAll();
+        return ResponseEntity.ok(animeService.listAll());
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime>findById(@PathVariable long id){
+        log.info(dateUltil.formatLocalDateToDatebaseStylw(LocalDateTime.now()));
+        return ResponseEntity.ok(animeService.findById(id));
     }
 }
--- src/main/java/com/f3pro/springboot2/service/AnimeService.java
+++ src/main/java/com/f3pro/springboot2/service/AnimeService.java
@@ -1,16 +1,26 @@
 package com.f3pro.springboot2.service;
 
 import com.f3pro.springboot2.domain.Anime;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Service
 public class AnimeService {
 
+    private List<Anime> animes = List.of(new Anime(1L,"aa"), new Anime(2L,"Berserk"));
+
     public List<Anime> listAll(){
+        return animes;
 
-        return List.of(new Anime(1L,"aa"), new Anime(2L,"Berserk"));
+    }
 
+    public Anime findById(long id){
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.BAD_REQUEST,"Anime not Found"));
     }
 }
